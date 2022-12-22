2173267c752d0594b63d571767d87ccc1ef4fe4b
Timur-001 <118300568+Timur-001@users.noreply.github.com>
2022-12-22 19:11:51 +0300

    Create DzD8.3.cs

---
+++ DzD8.3.cs
@@ -0,0 +1,52 @@
+// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+
+int[,] array1 = new int[2, 2];
+
+for  (int i = 0; i < array1.GerLength(0); i++)
+{
+      for  (int j = 0; i < array1.GerLength(1); i++)
+      {
+            array1[i, j] = new Random().Next(0, 12);
+            Console.Write(array1[i, j] + " ");
+            
+      }
+      Console.WtiteLine();
+}
+Console.WtiteLine("_________________");
+
+int[,] array2 = new int[2, 2];
+
+for (int i = 0; i < array2.GetLength(0); i++)
+{
+    for (int j = 0; j < array2.GetLength(1); j++)
+    {
+        array2[i, j] = new Random().Next(0, 12);
+        Console.Write(array2[i, j] + " ");
+    
+    }
+    Console.WtiteLine();
+}
+Console.WtiteLine("______________");
+
+int[,] array3 = new int[2, 2];
+
+for (int i = 0; i < array3.GetLength(0); i++)
+{
+     for (int j = 0; j < array3.GetLength(1); j++)
+     {
+          int sum = 0;
+          for (int k = 0; k < array1.GetLength(1); k++)
+          {
+              sum += array1[i, k] * array2[k, j];
+          }
+          Console.Write((array3[i, j] = sum)+ " ");
+          
+      }
+      Console.WriteLine();
+}
